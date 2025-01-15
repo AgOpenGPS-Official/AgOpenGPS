10d8ecbc3ddca4596a90a711e153de89e0d1113c
Joost van Rooijen <joost.rzz@gmail.com>
2025-01-15 12:14:31 +0100

    Add and check Pgn252AutoSteerSettings before removing CPGN_FC

--- SourceCode/GPS/Forms/PGN.Designer.cs
+++ SourceCode/GPS/Forms/PGN.Designer.cs
@@ -449,6 +449,7 @@ namespace AgOpenGPS
         public Pgn254AutoSteerData pgn254 = new Pgn254AutoSteerData();
 
         public CPGN_FC p_252 = new CPGN_FC();
+        public Pgn252AutoSteerSettings pgn252 = new Pgn252AutoSteerSettings();
 
         /// <summary>
         /// autoSteerConfig PGN - 251 - FB
