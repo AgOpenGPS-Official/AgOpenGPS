de83866b24f21965d9e7a12e1de6b9b04e400427
richardklasens <richardklasens@icloud.com>
2025-02-03 15:36:59 +0100

    removed duplicated JCB Icons

--- SourceCode/GPS/Properties/Resources.Designer.cs
+++ SourceCode/GPS/Properties/Resources.Designer.cs
@@ -2290,26 +2290,6 @@ namespace AgOpenGPS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap JCB1 {
-            get {
-                object obj = ResourceManager.GetObject("JCB1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap JCB2 {
-            get {
-                object obj = ResourceManager.GetObject("JCB2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap JobActive {
             get {
                 object obj = ResourceManager.GetObject("JobActive", resourceCulture);
