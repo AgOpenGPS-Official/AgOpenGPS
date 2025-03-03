3bade73674e0c0ffe0b8bd5601b94de7fd77c02e
Joost van Rooijen <joost.rzz@gmail.com>
2025-03-04 00:18:27 +0100

    Review comments

--- SourceCode/AgOpenGPS.Core/Models/Base/LocalPlane.cs
+++ SourceCode/AgOpenGPS.Core/Models/Base/LocalPlane.cs
@@ -2,14 +2,15 @@
 
 namespace AgOpenGPS.Core.Models
 {
+    // An instance of LocalPlane defines the origin and the meaning of a local coordinate
+    // system that uses Northing and Easting coordinates.
     public class LocalPlane
     {
-        const double degreesToRadians = 2.0 * Math.PI / 360.0;
-        //used to offset the antenna position to compensate for drift
+        // Used to offset the antenna position to compensate for drift
         static public GeoDelta FixDelta = new GeoDelta(0, 0);
 
-        private double _mPerDegreeLat;
-        private double _mPerDegreeLon;
+        private double _metersPerDegreeLat;
+        private double _metersPerDegreeLon;
 
         public LocalPlane(Wgs84 origin)
         {
@@ -22,28 +23,30 @@ namespace AgOpenGPS.Core.Models
         public GeoCoord ConvertWgs84ToGeoCoord(Wgs84 latLon)
         {
             return new GeoCoord(
-                (latLon.Latitude - Origin.Latitude) * _mPerDegreeLat,
-                (latLon.Longitude - Origin.Longitude) * _mPerDegreeLon);
+                (latLon.Latitude - Origin.Latitude) * _metersPerDegreeLat,
+                (latLon.Longitude - Origin.Longitude) * _metersPerDegreeLon);
         }
 
         public Wgs84 ConvertGeoCoordToWgs84(GeoCoord geoCoord)
         {
-            double lat = (geoCoord.Northing / _mPerDegreeLat) + Origin.Latitude;
-            double lon = (geoCoord.Easting / _mPerDegreeLon) + Origin.Longitude;
+            double lat = (geoCoord.Northing / _metersPerDegreeLat) + Origin.Latitude;
+            double lon = (geoCoord.Easting / _metersPerDegreeLon) + Origin.Longitude;
             return new Wgs84(lat, lon);
         }
 
+        // see https://en.wikipedia.org/wiki/Geographic_coordinate_system#Latitude_and_longitude
         private void SetLocalMetersPerDegree()
         {
-            _mPerDegreeLat = 111132.92
-                - 559.82 * Math.Cos(2.0 * Origin.Latitude * degreesToRadians)
-                + 1.175 * Math.Cos(4.0 * Origin.Latitude * degreesToRadians)
-                - 0.0023 * Math.Cos(6.0 * Origin.Latitude * degreesToRadians);
+            double originLatInRad = Units.DegreesToRadians(Origin.Latitude);
+            _metersPerDegreeLat = 111132.92
+                - 559.82 * Math.Cos(2.0 * originLatInRad)
+                + 1.175 * Math.Cos(4.0 * originLatInRad)
+                - 0.0023 * Math.Cos(6.0 * originLatInRad);
 
-            _mPerDegreeLon =
-                111412.84 * Math.Cos(Origin.Latitude * degreesToRadians)
-                - 93.5 * Math.Cos(3.0 * Origin.Latitude * degreesToRadians)
-                + 0.118 * Math.Cos(5.0 * Origin.Latitude * degreesToRadians);
+            _metersPerDegreeLon =
+                111412.84 * Math.Cos(originLatInRad)
+                - 93.5 * Math.Cos(3.0 * originLatInRad)
+                + 0.118 * Math.Cos(5.0 * originLatInRad);
         }
 
     }
