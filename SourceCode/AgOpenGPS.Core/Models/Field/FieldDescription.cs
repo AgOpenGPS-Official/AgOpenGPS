e2a9aed1b5221fe2c07998aa550da4748f3deaba
Joost van Rooijen <joost.rzz@gmail.com>
2025-03-06 22:28:14 +0100

    Make FieldDescription according to comments

--- SourceCode/AgOpenGPS.Core/Models/Field/FieldDescription.cs
+++ SourceCode/AgOpenGPS.Core/Models/Field/FieldDescription.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics.Tracing;
+﻿using AgLibrary.Logging;
+using AgOpenGPS.Core.Streamers;
+using System;
+using System.Diagnostics.Tracing;
+using System.IO;
 
 namespace AgOpenGPS.Core.Models
 {
@@ -6,15 +10,33 @@ namespace AgOpenGPS.Core.Models
     // Used to build a FieldDescriptionViewModel to display a row in a table of fields
     public class FieldDescription
     {
-        public FieldDescription(string name)
+        private readonly DirectoryInfo _fieldDirectory;
+        public FieldDescription(DirectoryInfo fieldDirectory)
         {
-            Name = name;
+            _fieldDirectory = fieldDirectory;
+            try
+            {
+                var overview = new OverviewStreamer().Read(fieldDirectory);
+                Wgs84Start = overview.Start;
+            }
+            catch (Exception )
+            {
+                Log.EventWriter("Field (" + _fieldDirectory.Name + ") file (Field.txt) could not be read.");
+            }
+            try
+            {
+                var boundary = new BoundaryStreamer().Read(fieldDirectory);
+                Area = boundary.Area;
+            }
+            catch (Exception)
+            {
+                Log.EventWriter("Field (" + _fieldDirectory.Name + ") file (Boundary.txt) could not be read.");
+            }
         }
 
-        public string Name { get; }
+        public string Name => _fieldDirectory.Name;
         public Wgs84? Wgs84Start { get; set; } // No value indicates error in Field.txt file
         public double? Area { get; set; } // In Square meters. No value indicates error in reading Boundary file
-
     }
 
 }
