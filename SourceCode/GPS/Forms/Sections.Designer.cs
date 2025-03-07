cfbeef156b220e11d1eda5e04965b1cf3bf25fc0
steran82 <stefan.niedermeyer@gmx.de>
2025-03-07 20:32:16 +0100

    fixed bug: index out of bound when clicking the green or yellow symbol for marking

--- SourceCode/GPS/Forms/Sections.Designer.cs
+++ SourceCode/GPS/Forms/Sections.Designer.cs
@@ -20,20 +20,25 @@ namespace AgOpenGPS
 
         private void markAsWorkedTrack()
         {
-            //is it an AB Line or a Curve?
-            if (trk.gArr[this.trk.idx].mode == TrackMode.AB)
+            //only mark it if there was a track selected?
+            if (this.trk.idx >= 0)
             {
-                //AB Line
-                if (!this.trk.gArr[this.trk.idx].workedTracks.Contains(this.ABLine.howManyPathsAway))
-                    this.trk.gArr[this.trk.idx].workedTracks.Add(this.ABLine.howManyPathsAway);
 
-            }
-            else if (trk.gArr[this.trk.idx].mode == TrackMode.Curve)
-            {
-                //Curve
-                if (!this.trk.gArr[this.trk.idx].workedTracks.Contains(this.curve.howManyPathsAway))
-                    this.trk.gArr[this.trk.idx].workedTracks.Add(this.curve.howManyPathsAway);
+                //is it an AB Line or a Curve?
+                if (trk.gArr[this.trk.idx].mode == TrackMode.AB)
+                {
+                    //AB Line
+                    if (!this.trk.gArr[this.trk.idx].workedTracks.Contains(this.ABLine.howManyPathsAway))
+                        this.trk.gArr[this.trk.idx].workedTracks.Add(this.ABLine.howManyPathsAway);
 
+                }
+                else if (trk.gArr[this.trk.idx].mode == TrackMode.Curve)
+                {
+                    //Curve
+                    if (!this.trk.gArr[this.trk.idx].workedTracks.Contains(this.curve.howManyPathsAway))
+                        this.trk.gArr[this.trk.idx].workedTracks.Add(this.curve.howManyPathsAway);
+
+                }
             }
         }
 
