8cfe3d98b39e29cd52f08b1d2a32adb557b9ae45
Joost van Rooijen <joost.rzz@gmail.com>
2025-03-11 17:46:43 +0100

    First viewmodels for field selection

---
+++ SourceCode/AgOpenGPS.Core/ViewModels/StartNewFieldViewModels/FieldTableViewModel.cs
@@ -0,0 +1,81 @@
+﻿using AgLibrary.ViewModels;
+using AgOpenGPS.Core.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace AgOpenGPS.Core.ViewModels
+{
+    public class FieldTableViewModel : ViewModel
+    {
+        protected readonly ApplicationModel _appModel;
+        private ObservableCollection<FieldDescriptionViewModel> _fieldDescriptions;
+        protected FieldDescriptionViewModel _localSelectedField;
+
+        public FieldTableViewModel(ApplicationModel appModel)
+        {
+            _appModel = appModel;
+            SelectFieldCommand = new RelayCommand(SelectField);
+            SortCommand = new RelayCommand(Sort);
+        }
+
+        public ICommand SelectFieldCommand { get; }
+        public ICommand SortCommand { get; }
+
+        public ObservableCollection<FieldDescriptionViewModel> FieldDescriptionViewModels
+        {
+            get { return _fieldDescriptions; }
+            set
+            {
+                _fieldDescriptions = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // The field that is selected in the table (if any). 
+        // Probably different from the field that is currently selected in the application
+
+        public FieldDescriptionViewModel LocalSelectedField
+        {
+            get { return _localSelectedField; }
+            set
+            {
+                if (value != _localSelectedField)
+                {
+                    _localSelectedField = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public void UpdateFields()
+        {
+            ObservableCollection<FieldDescriptionViewModel> viewModels = new ObservableCollection<FieldDescriptionViewModel>();
+            var descriptions = _appModel.Fields.GetFieldDescriptions();
+            foreach (FieldDescription fd in descriptions)
+            {
+                FieldDescriptionViewModel viewModel = new FieldDescriptionViewModel(
+                    fd.FieldDirectory,
+                    fd.Area.HasValue ? fd.Area.Value.ToString() : "Error",
+                    fd.Wgs84Start.HasValue ? _appModel.CurrentLatLon.DistanceInKiloMeters(fd.Wgs84Start.Value).ToString() : "Error");
+                viewModels.Add(viewModel);
+            }
+            FieldDescriptionViewModels = viewModels;
+        }
+
+        protected virtual void SelectField()
+        {
+            var selectedField = LocalSelectedField;
+            if (null != selectedField)
+            {
+                LocalSelectedField = null;
+                _appModel.Fields.SelectField(selectedField.DirectoryInfo);
+            }
+        }
+
+        private void Sort()
+        {
+            // TODO implement sorting
+        }
+    }
+
+}
---
+++ SourceCode/AgOpenGPS.Core/ViewModels/StartNewFieldViewModels/StartNewFieldViewModel.cs
@@ -0,0 +1,111 @@
+﻿using AgLibrary.ViewModels;
+using AgOpenGPS.Core.Interfaces;
+using AgOpenGPS.Core.Models;
+using AgOpenGPS.Core.Streamers;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
+
+namespace AgOpenGPS.Core.ViewModels
+{
+    public class StartNewFieldViewModel
+    {
+        private readonly ApplicationModel _appModel;
+        private IPanelPresenter _panelPresenter;
+
+        private SelectNearFieldViewModel _selectNearFieldViewModel;
+        private CreateFromExistingFieldViewModel _createFromExistingFieldViewModel;
+        private SelectFieldViewModel _selectFieldViewModel;
+
+        public StartNewFieldViewModel(ApplicationModel appModel)
+        {
+            _appModel = appModel;
+            StartSelectNearFieldCommand = new RelayCommand(StartSelectNearField);
+            StartCreateFieldFromExistingCommand = new RelayCommand(StartCreateFieldFromExisting);
+            StartSelectFieldCommand = new RelayCommand(StartSelectField);
+            CancelCommand = new RelayCommand(Cancel);
+        }
+
+        public IPanelPresenter PanelPresenter
+        {
+            set {
+                _panelPresenter = value;
+            }
+        }
+
+        public SelectNearFieldViewModel SelectNearFieldViewModel
+        {
+            get
+            {
+                if (_selectNearFieldViewModel == null)
+                {
+                    _selectNearFieldViewModel =
+                        new SelectNearFieldViewModel(_appModel, _panelPresenter);
+                }
+                return _selectNearFieldViewModel;
+            }
+        }
+
+        public CreateFromExistingFieldViewModel CreateFromExistingFieldViewModel
+        {
+            get
+            {
+                if (_createFromExistingFieldViewModel == null)
+                {
+                    _createFromExistingFieldViewModel =
+                        new CreateFromExistingFieldViewModel(_appModel, _panelPresenter);
+                }
+                return _createFromExistingFieldViewModel;
+            }
+        }
+
+        public SelectFieldViewModel SelectFieldViewModel
+        {
+            get
+            {
+                if (_selectFieldViewModel == null)
+                {
+                    _selectFieldViewModel = new SelectFieldViewModel(_appModel, _panelPresenter);
+                }
+                return _selectFieldViewModel;
+            }
+        }
+
+        public ICommand StartSelectNearFieldCommand { get;}
+        public ICommand StartCreateFieldFromExistingCommand { get; }
+        public ICommand StartSelectFieldCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public string CurrentFieldName => _appModel.Fields.CurrentFieldName;
+
+        private void StartSelectNearField()
+        {
+            // TODO implement different behaviour if number of fields is 0 or 1
+            _panelPresenter.CloseStartNewFieldDialog();
+            SelectNearFieldViewModel.UpdateFields();
+            _panelPresenter.ShowSelectNearFieldDialog(SelectNearFieldViewModel);
+        }
+
+        private void StartCreateFieldFromExisting()
+        {
+            // TODO implement different behaviour if number of fields is 0 or 1
+            _panelPresenter.CloseStartNewFieldDialog();
+            CreateFromExistingFieldViewModel.UpdateFields();
+            _panelPresenter.ShowCreateFromExistingFieldDialog(CreateFromExistingFieldViewModel);
+        }
+
+        private void StartSelectField()
+        {
+            // TODO implement different behaviour if number of fields is 0 or 1
+            _panelPresenter.CloseStartNewFieldDialog();
+            SelectFieldViewModel.UpdateFields();
+            _panelPresenter.ShowSelectFieldDialog(SelectFieldViewModel);
+        }
+
+        private void Cancel()
+        {
+            _panelPresenter.CloseStartNewFieldDialog();
+        }
+
+    }
+}
