31b354d89b663abd940a7bc3a7468568b760a91a
Joost van Rooijen <joost.rzz@gmail.com>
2025-04-02 20:02:01 +0200

    Simply let the view display the number of fields that fit the table

--- SourceCode/AgOpenGPS.Core/ViewModels/StartNewFieldViewModels/NearFieldTableViewModel.cs
+++ SourceCode/AgOpenGPS.Core/ViewModels/StartNewFieldViewModels/NearFieldTableViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using AgOpenGPS.Core.Models;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace AgOpenGPS.Core.ViewModels
 {
@@ -13,7 +12,7 @@ namespace AgOpenGPS.Core.ViewModels
 
         public new void UpdateFields()
         {
-            Collection<FieldDescriptionViewModel> distanceViewModels = new Collection<FieldDescriptionViewModel>();
+            Collection<FieldDescriptionViewModel> viewModels = new Collection<FieldDescriptionViewModel>();
             var descriptions = _appModel.Fields.GetFieldDescriptions();
             foreach (FieldDescription description in descriptions)
             {
@@ -22,15 +21,12 @@ namespace AgOpenGPS.Core.ViewModels
                     FieldDescriptionViewModel viewModel = new FieldDescriptionViewModel(
                         description,
                         _appModel.CurrentLatLon);
-                    distanceViewModels.Add(viewModel);
+                    viewModels.Add(viewModel);
                 }
             }
-            Collection<FieldDescriptionViewModel> nearFieldViewModels
-                = new Collection<FieldDescriptionViewModel>(
-                    distanceViewModels.OrderBy(vm => vm.DistanceViewModel.DistanceInKm).Take(6).ToList());
             // The Winforms views do not update when elements inside the ObservableCollection are changed.
             // Therefore change the ObservableCollection as a whole.
-            FieldDescriptionViewModels = nearFieldViewModels;
+            FieldDescriptionViewModels = viewModels;
         }
 
     }
