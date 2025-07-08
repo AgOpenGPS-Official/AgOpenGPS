29450283791ed5fa458cd98308aa18130aa9b946
richardklasens <richardklasens@icloud.com>
2025-07-08 10:47:49 +0200

    Initial FormDialog

---
+++ SourceCode/GPS/Forms/FormDialog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AgOpenGPS.Forms
+{
+    public partial class FormDialog : Form
+    {
+        public FormDialog(string message, string title = "Title", bool showCancel = true)
+        {
+            InitializeComponent();
+            lblMessage.Text = message;
+            lblTitle.Text = title;
+            btnCancel.Visible = showCancel;
+        }
+
+
+        public static DialogResult Show(string title, string message, MessageBoxButtons buttons = MessageBoxButtons.OKCancel)
+        {
+            bool showCancel = (buttons == MessageBoxButtons.OKCancel || buttons == MessageBoxButtons.YesNo);
+            return new FormDialog(message, title, showCancel).ShowDialog();
+        }
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
+
