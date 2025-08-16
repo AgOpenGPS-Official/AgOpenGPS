9e710f6d65307d7f6d6b15e1f30b3612b70f72fe
FantasyTeddy <matthiasbaer@bluewin.ch>
2025-08-16 15:29:57 +0200

    Replaces WebEye camera control with OpenCvSharp

--- SourceCode/GPS/Forms/FormWebCam.cs
+++ SourceCode/GPS/Forms/FormWebCam.cs
@@ -1,78 +1,59 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
-using WebEye.Controls.WinForms.WebCameraControl;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 
 namespace AgOpenGPS
 {
     public partial class FormWebCam : Form
     {
+        private readonly VideoCapture _capture = new VideoCapture();
+
         public FormWebCam()
         {
             InitializeComponent();
         }
 
-        private class ComboBoxItem
-        {
-            public ComboBoxItem(WebCameraId id)
-            {
-                _id = id;
-            }
-
-            private readonly WebCameraId _id;
-
-            public WebCameraId Id => _id;
-
-            public override string ToString()
-            {
-                // Generates the text shown in the combo box.
-                return _id.Name;
-            }
-        }
-
         private void FormWebCam_Load(object sender, EventArgs e)
         {
-            foreach (WebCameraId camera in webCameraControl1.GetVideoCaptureDevices())
-            {
-                comboBox1.Items.Add(new ComboBoxItem(camera));
-            }
-
-            if (comboBox1.Items.Count > 0)
-            {
-                comboBox1.SelectedItem = comboBox1.Items[0];
-            }
+            webCamBackgroundWorker.RunWorkerAsync();
         }
 
-        private void UpdateButtons()
+        private void FormWebCam_FormClosing(object sender, FormClosingEventArgs e)
         {
-            startButton.Enabled = comboBox1.SelectedItem != null;
-            stopButton.Enabled = webCameraControl1.IsCapturing;
-            //imageButton.Enabled = webCameraControl1.IsCapturing;
+            webCamBackgroundWorker.CancelAsync();
+            _capture.Dispose();
         }
 
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        private void webCamBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            UpdateButtons();
-        }
+            _capture.Open(0);
 
-        private void startButton_Click_1(object sender, EventArgs e)
-        {
-            ComboBoxItem i = (ComboBoxItem)comboBox1.SelectedItem;
-
-            try
+            if (!_capture.IsOpened())
             {
-                webCameraControl1.StartCapture(i.Id);
+                Close();
+                return;
             }
-            finally
+
+            while (!webCamBackgroundWorker.CancellationPending)
             {
-                UpdateButtons();
+                using (var frameMat = _capture.RetrieveMat())
+                {
+                    var bitmap = BitmapConverter.ToBitmap(frameMat);
+                    webCamBackgroundWorker.ReportProgress(0, bitmap);
+                }
+                Thread.Sleep(100);
             }
         }
 
-        private void stopButton_Click_1(object sender, EventArgs e)
+        private void webCamBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            webCameraControl1.StopCapture();
-
-            UpdateButtons();
+            var bitmap = (Bitmap)e.UserState;
+            webCamPictureBox.Image?.Dispose();
+            webCamPictureBox.Image = bitmap;
         }
     }
 }
