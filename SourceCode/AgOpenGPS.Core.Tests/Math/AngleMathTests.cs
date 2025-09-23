d2b14d912321bcc949d23f9c6f761ccc4f2e7ef5
richardklasens <richardklasens@icloud.com>
2025-09-24 00:51:40 +0200

    New Math Helpers

---
+++ SourceCode/AgOpenGPS.Core.Tests/Math/AngleMathTests.cs
@@ -0,0 +1,48 @@
+﻿// AgOpenGPS.Core.Tests/Math/AngleMathTests.cs
+// Purpose: Smoke tests for AngleMath helpers (NUnit).
+using AgOpenGPS.Core.Mathx;
+using NUnit.Framework;
+using System;
+
+namespace AgOpenGPS.Core.Tests.Mathx
+{
+    [TestFixture]
+    public class AngleMathTests
+    {
+        [Test]
+        public void ToRadians_ToDegrees_Roundtrip()
+        {
+            var deg = 123.45;
+            var rad = AngleMath.ToRadians(deg);
+            var back = AngleMath.ToDegrees(rad);
+            Assert.That(back, Is.EqualTo(deg).Within(1e-12));
+        }
+
+        [Test]
+        public void NormalizePositive_WrapsInto_0_2Pi()
+        {
+            Assert.That(AngleMath.NormalizePositive(0.0), Is.EqualTo(0.0).Within(1e-12));
+            Assert.That(AngleMath.NormalizePositive(-0.1), Is.GreaterThan(6.18)); // near 2π
+            Assert.That(AngleMath.NormalizePositive(7.0), Is.LessThan(1.0));      // wrapped
+        }
+
+        [Test]
+        public void ShortestDelta_IsSignedWithinMinusPiToPi()
+        {
+            double from = 0.0;
+            double to = Math.PI * 1.5; // 270 deg
+            var d = AngleMath.ShortestDelta(from, to);
+            Assert.That(d, Is.LessThan(0.0)); // rotate -90 deg is shortest
+            Assert.That(d, Is.EqualTo(-Math.PI / 2.0).Within(1e-12));
+        }
+
+        [Test]
+        public void AngleDiff_AbsoluteWithin_0_Pi()
+        {
+            var d1 = AngleMath.AngleDiff(0, Math.PI / 2);
+            var d2 = AngleMath.AngleDiff(AngleMath.TwoPi - 0.1, 0.1);
+            Assert.That(d1, Is.EqualTo(Math.PI / 2).Within(1e-12));
+            Assert.That(d2, Is.EqualTo(0.2).Within(1e-12));
+        }
+    }
+}
---
+++ SourceCode/AgOpenGPS.Core.Tests/Math/Distance2DTests.cs
@@ -0,0 +1,37 @@
+﻿// AgOpenGPS.Core.Tests/Math/Distance2DTests.cs
+// Purpose: Smoke tests for Distance2D helpers (NUnit).
+using AgOpenGPS.Core.Mathx;
+using AgOpenGPS.Core.Models;
+using NUnit.Framework;
+
+namespace AgOpenGPS.Core.Tests.Mathx
+{
+    [TestFixture]
+    public class Distance2DTests
+    {
+        [Test]
+        public void Distance_DirectVsGeoCoord_Agree()
+        {
+            var a = new GeoCoord(0.0, 0.0);   // (northing, easting)
+            var b = new GeoCoord(3.0, 4.0);   // distance should be 5.0
+
+            var d1 = Distance2D.Distance(a.Easting, a.Northing, b.Easting, b.Northing);
+            var d2 = Distance2D.Distance(a, b);
+
+            Assert.That(d1, Is.EqualTo(5.0).Within(1e-12));
+            Assert.That(d2, Is.EqualTo(5.0).Within(1e-12));
+        }
+
+        [Test]
+        public void DistanceSquared_Matches_SquareOfDistance()
+        {
+            var a = new GeoCoord(10.0, 10.0);
+            var b = new GeoCoord(13.0, 14.0);
+
+            var d = Distance2D.Distance(a, b);
+            var ds = Distance2D.DistanceSquared(a, b);
+
+            Assert.That(ds, Is.EqualTo(d * d).Within(1e-12));
+        }
+    }
+}
