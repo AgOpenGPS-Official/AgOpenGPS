84a31be9152ba90cc2a36efdd32cb248631cc11d
Xyntexx <21282105+Xyntexx@users.noreply.github.com>
2025-10-14 21:13:35 +0300

    Add red borders to forms and fix FormTimedMessage background color
    - FormTimedMessage: Use OldLace background with red border (original GPS style)
    - FormYes: Keep orange/peach background with red border for consistency
    - Both forms now use panel-based layout with 8px padding for red border effect

--- SourceCode/AgLibrary/Forms/FormTimedMessage.Designer.cs
+++ SourceCode/AgLibrary/Forms/FormTimedMessage.Designer.cs
@@ -29,13 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblMessage2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
+            // panel1
+            //
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.OldLace;
+            this.panel1.Controls.Add(this.lblMessage2);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 30, 30);
+            this.panel1.Size = new System.Drawing.Size(252, 131);
+            this.panel1.TabIndex = 2;
+            //
             // lblTitle
-            // 
+            //
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold);
             this.lblTitle.Location = new System.Drawing.Point(12, 20);
@@ -43,15 +58,9 @@
             this.lblTitle.Size = new System.Drawing.Size(115, 29);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Message";
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 3000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
+            //
             // lblMessage2
-            // 
+            //
             this.lblMessage2.AutoSize = true;
             this.lblMessage2.Font = new System.Drawing.Font("Tahoma", 18F);
             this.lblMessage2.Location = new System.Drawing.Point(75, 71);
@@ -59,34 +68,42 @@
             this.lblMessage2.Size = new System.Drawing.Size(127, 29);
             this.lblMessage2.TabIndex = 1;
             this.lblMessage2.Text = "Message 2";
-            // 
+            //
+            // timer1
+            //
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            //
             // FormTimedMessage
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(268, 147);
             this.ControlBox = false;
-            this.Controls.Add(this.lblMessage2);
-            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 14.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormTimedMessage";
-            this.Padding = new System.Windows.Forms.Padding(0, 0, 30, 30);
+            this.Padding = new System.Windows.Forms.Padding(8);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = string.Empty;
             this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lblMessage2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SourceCode/AgLibrary/Forms/FormYes.Designer.cs
+++ SourceCode/AgLibrary/Forms/FormYes.Designer.cs
@@ -28,23 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.lblMessage2 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.btnSerialOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
+            // panel1
+            //
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnSerialOK);
+            this.panel1.Controls.Add(this.lblTitle);
+            this.panel1.Controls.Add(this.lblMessage2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(582, 336);
+            this.panel1.TabIndex = 98;
+            //
+            // lblTitle
+            //
+            this.lblTitle.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(38, 2);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(528, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTitle.Visible = false;
+            //
             // lblMessage2
-            // 
+            //
             this.lblMessage2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold);
-            this.lblMessage2.Location = new System.Drawing.Point(46, 23);
+            this.lblMessage2.Location = new System.Drawing.Point(38, 52);
             this.lblMessage2.Name = "lblMessage2";
-            this.lblMessage2.Size = new System.Drawing.Size(528, 280);
+            this.lblMessage2.Size = new System.Drawing.Size(528, 200);
             this.lblMessage2.TabIndex = 1;
             this.lblMessage2.Text = "Message 2";
             this.lblMessage2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+            //
             // btnSerialOK
-            // 
+            //
             this.btnSerialOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnSerialOK.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnSerialOK.DialogResult = System.Windows.Forms.DialogResult.OK;
@@ -53,15 +80,15 @@
             this.btnSerialOK.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSerialOK.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnSerialOK.Image = global::AgLibrary.Properties.Resources.OK64;
-            this.btnSerialOK.Location = new System.Drawing.Point(490, 266);
+            this.btnSerialOK.Location = new System.Drawing.Point(482, 258);
             this.btnSerialOK.Name = "btnSerialOK";
             this.btnSerialOK.Size = new System.Drawing.Size(105, 90);
             this.btnSerialOK.TabIndex = 96;
             this.btnSerialOK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnSerialOK.UseVisualStyleBackColor = true;
-            // 
+            //
             // btnCancel
-            // 
+            //
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
@@ -70,37 +97,39 @@
             this.btnCancel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancel.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnCancel.Image = global::AgLibrary.Properties.Resources.Cancel64;
-            this.btnCancel.Location = new System.Drawing.Point(363, 266);
+            this.btnCancel.Location = new System.Drawing.Point(355, 258);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(105, 90);
             this.btnCancel.TabIndex = 97;
             this.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Visible = false;
-            // 
+            //
             // FormYes
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.Red;
             this.ClientSize = new System.Drawing.Size(598, 352);
             this.ControlBox = false;
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSerialOK);
-            this.Controls.Add(this.lblMessage2);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Tahoma", 14.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormYes";
+            this.Padding = new System.Windows.Forms.Padding(8);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = string.Empty;
             this.TopMost = true;
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTitle;
         private System.Windows.Forms.Label lblMessage2;
         private System.Windows.Forms.Button btnSerialOK;
         private System.Windows.Forms.Button btnCancel;
