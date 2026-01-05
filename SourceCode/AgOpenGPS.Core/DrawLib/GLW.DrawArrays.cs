4de43dd71e50b5d6c767284f0d65fc51d23f63f1
Joost van Rooijen <joost.rzz@gmail.com>
2026-01-05 01:04:14 +0100

    A Vertex2Array only worked once. (Some code was misplaced in constructor)

---
+++ SourceCode/AgOpenGPS.Core/DrawLib/GLW.DrawArrays.cs
@@ -0,0 +1,37 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace AgOpenGPS.Core.DrawLib
+{
+    // GLW is short for GL Wrapper.
+    // Please use this class in stead of direct calls to functions in the GL toolkit.
+    public static partial class GLW
+    {
+
+        // Inlined by the compiler, so no function call overhead
+        public static void DrawLineLoopArrays(Vertex2Array array)
+        {
+            DrawArrays(PrimitiveType.LineLoop, array);
+        }
+
+        // Inlined by the compiler, so no function call overhead
+        public static void DrawLineStripArrays(Vertex2Array array)
+        {
+            DrawArrays(PrimitiveType.LineStrip, array);
+        }
+
+        private static void DrawArrays(PrimitiveType primitiveType, Vertex2Array vertex2Array)
+        {
+            if (vertex2Array == null) return;
+            vertex2Array.BindBuffer();
+
+            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Double, false, 0, 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.DrawArrays(primitiveType, 0, vertex2Array.Length);
+
+            GL.DisableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+
+    }
+}
