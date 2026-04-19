7428912383079ec0df9bbe2e17f155b6112ba82e
vix <viktor.kovac86@gmail.com>
2026-04-19 14:34:07 +0200

    Add CereaBridge device stubs

---
+++ SourceCode/CereaBridge/BridgeDevices.cs
@@ -0,0 +1,22 @@
+namespace CereaBridge
+{
+    internal sealed partial class BridgeService
+    {
+        private void OpenDevices()
+        {
+        }
+
+        private void CloseDevices()
+        {
+        }
+
+        private int ReadCounts()
+        {
+            return _wasOffset;
+        }
+
+        private void ReadImu(ref short heading16, ref short roll16)
+        {
+        }
+    }
+}
