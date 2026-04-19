6f4fd8e99dde4ff4c8f726b6976686c7eb1d78b8
vix <viktor.kovac86@gmail.com>
2026-04-19 14:31:50 +0200

    Add CereaBridge service skeleton

---
+++ SourceCode/CereaBridge/BridgeService.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Phidget22.Devices;
+using Tinkerforge;
+
+namespace CereaBridge
+{
+    internal sealed partial class BridgeService : IDisposable
+    {
+        private readonly BridgeConfig _cfg;
+        private readonly UdpClient _listenPrimary;
+        private readonly UdpClient _listenFallback;
+        private readonly UdpClient _sender;
+        private readonly IPEndPoint _agioEndpoint;
+        private readonly Timer _telemetryTimer;
+        private readonly Timer _helloTimer;
+
+        private DCMotor _motor;
+        private Encoder _encoder;
+        private IPConnection _ipcon;
+        private BrickIMUV2 _imu;
+
+        private volatile bool _running;
+        private double _desiredAngleDeg;
+        private bool _autosteerEnabled;
+        private double _speedKph;
+        private byte _kp = 20;
+        private byte _highPwm = 120;
+        private byte _minPwm = 25;
+        private byte _countsPerDegree = 30;
+        private int _wasOffset;
+        private byte _minSpeedX10 = 5;
+        private int _lastPwm;
+
+        public BridgeService(BridgeConfig cfg)
+        {
+            _cfg = cfg;
+            _listenPrimary = new UdpClient(cfg.ListenPort);
+            _listenFallback = new UdpClient(cfg.ListenPortFallback);
+            _sender = new UdpClient();
+            _agioEndpoint = new IPEndPoint(IPAddress.Parse(cfg.AgioHost), cfg.AgioPort);
+            _telemetryTimer = new Timer(OnTelemetryTick, null, Timeout.Infinite, Timeout.Infinite);
+            _helloTimer = new Timer(OnHelloTick, null, Timeout.Infinite, Timeout.Infinite);
+            _wasOffset = cfg.WasOffsetFallback;
+        }
+
+        public void Start()
+        {
+            _running = true;
+            OpenDevices();
+            BeginReceive(_listenPrimary);
+            BeginReceive(_listenFallback);
+            _telemetryTimer.Change(_cfg.TelemetryPeriodMs, _cfg.TelemetryPeriodMs);
+            _helloTimer.Change(_cfg.HelloPeriodMs, _cfg.HelloPeriodMs);
+        }
+
+        public void Dispose()
+        {
+            _running = false;
+            _telemetryTimer.Dispose();
+            _helloTimer.Dispose();
+            CloseDevices();
+            _listenPrimary.Dispose();
+            _listenFallback.Dispose();
+            _sender.Dispose();
+        }
+    }
+}
